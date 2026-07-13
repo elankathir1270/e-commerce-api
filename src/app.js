25b2e98ffff3d05ed5632b15df9ff9d345f9611d
elankathir <esekar@diligentsquad.com>
2026-07-13 15:58:57 +0530

    add routes to inventory

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const productRouter = require('./routes/product.routes');
 const reviewRouter = require('./routes/review.routes');
+const inventoryRouter = require('./routes/inventory.routes');
 
 const app = express();
 
@@ -8,8 +9,9 @@ const app = express();
 app.use(express.json());
 
 //add routes for app
-app.use('/api/v1/products',productRouter);
+app.use("/api/v1/products",productRouter);
 app.use("/api/v1/reviews", reviewRouter);
+app.use("api/v1/inventory", inventoryRouter);
 
 
 module.exports = app;
---
+++ src/routes/inventory.routes.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const inventoryController = require('./../controllers/inventory.controller');
+const validate = require('./../middlewares/validate.middleware');
+const {
+  increaseStockSchema,
+  decreaseStockSchema,
+  adjustStockSchema,
+  reserveStockSchema,
+  releaseReservationSchema,
+  consumeReservationSchema,
+} = require('./../validators/inventory.validator');
+
+const inventoryRouter = express.Router();
+
+//Stock Management
+inventoryRouter.route("/:productId/increase")
+.patch(validate(increaseStockSchema),inventoryController.increaseStock);
+
+inventoryRouter.route("/:productId/decrease")
+.patch(validate(decreaseStockSchema),inventoryController.decreaseStock);
+
+inventoryRouter.route("/:productId/adjust")
+.patch(validate(adjustStockSchema),inventoryController.adjustStock);
+
+//Reservation Management
+inventoryRouter.route("/:productId/reserve")
+.post(validate(reserveStockSchema), inventoryController.reserveStock);
+
+inventoryRouter.route("/reservations/:reservationId/release")
+.patch(validate(releaseReservationSchema), inventoryController.releaseReservation);
+
+inventoryRouter.route("/reservations/:reservationId/consume")
+.patch(validate(consumeReservationSchema),inventoryController.consumeReservation);
+
+
+/**
+-- Increase, decrease, and adjust stock → Admin or Warehouse roles only.
+-- Reserve and consume inventory → Typically called internally by the Cart or Checkout service, not exposed as public APIs.
+-- Release reservation → Triggered by Cart, Checkout, or the background expiry job.
+
+
+Method	Endpoint	                                    Description
+PATCH	/inventory/:productId/increase	                Increase stock
+PATCH	/inventory/:productId/decrease	                Decrease stock
+PATCH	/inventory/:productId/adjust	                Set stock to an exact quantity
+POST	/inventory/:productId/reserve	                Reserve inventory
+PATCH	/inventory/reservations/:reservationId/release	Release reservation
+PATCH	/inventory/reservations/:reservationId/consume	Convert reservation into a sale
+
+ */
+
+
+module.exports(inventoryRouter);
